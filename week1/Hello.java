ec4c2f1b693c6bdd0b96535295199dd7663e2900
Ishan <isan91798@gmail.com>
2024-04-14 14:41:54 +0545

    Week 1 Task 1 comment

---
+++ week1/Hello.java
@@ -0,0 +1,10 @@
+package week1;
+
+public class Hello {
+    /*1. Write a program that prints Hello World! to the console. Save the program as Hello.java. */
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+
